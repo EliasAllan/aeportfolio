157729450e5c118137ab23d6b6d985fffe9595e0
AllanEl <allanrnelias@gmail.com>
2023-02-04 00:37:55 -0500

    started navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Navbar() {
+    
+    return (
+       <nav>
+        <a href="#Aboutme">About me</a>
+        <a href="#work">Work</a>
+        <a href="#contactinfo">Contact me</a>   
+      </nav>
+    )
+}
+
+export default Navbar;
