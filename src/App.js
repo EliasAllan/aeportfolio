051af75a3358ded5a7179744cbae2ed260b4f471
AllanEl <allanrnelias@gmail.com>
2023-02-09 18:18:15 -0500

    added resume link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Header from "./components/Header";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route index element={<About />} />
             <Route path="projects" element={<Projects />} />
             <Route path="contact" element={<Contact/>} />
+            <Route path="resume" element={<Resume/>} />
         </Route>
         </Routes>
         <Footer />
---
+++ src/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Resume() {
+
+  return (
+    <div id="resume-link">
+    <Link to="https://docs.google.com/document/d/1RKSWOc18Bm8h0nnGLRxivGJdHxHOTb7QcF83kEY5db0/edit?usp=sharing" href="true">Click here to see and/or download my resume</Link>
+    </div> 
+  );
+}
+
+export default Resume;
