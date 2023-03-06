7e56bbdcaed5f1df17ea49fcf1cd586a1d21190a
AllanEl <allanrnelias@gmail.com>
2023-03-06 03:27:09 -0500

    projects wrapped in a swipe function

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,12 +6,34 @@ import futureblogSc from "../assets/homepage.png";
 import horiseonSc from "../assets/horiseon.png";
 import studyBuddySc from "../assets/study-buddy.png";
 import jateSc from "../assets/jate.png";
-
-
-
 import { Link } from "react-router-dom";
+
+
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+
 function Projects() {
   const projects = [
+    {
+      title: "Study buddy",
+      link: "https://study-buddy-dream-team.herokuapp.com/",
+      gitLink: "https://github.com/EliasAllan/study-buddy",
+      img: (
+        <Link to="https://study-buddy-dream-team.herokuapp.com/" href="true">
+          <img
+            className="project-screenshot"
+            src={studyBuddySc}
+            alt="study-buddy-screenshot"
+            width="100%"
+            border="20px"
+       
+          />
+        </Link>
+      ),
+      
+    },
     {
       title: "Pet Adoption",
       link: "https://pet-adoption-store.herokuapp.com/",
@@ -96,32 +118,20 @@ function Projects() {
       ),
       
     },
-    {
-      title: "Study buddy",
-      link: "https://study-buddy-dream-team.herokuapp.com/",
-      gitLink: "https://github.com/EliasAllan/study-buddy",
-      img: (
-        <Link to="https://study-buddy-dream-team.herokuapp.com/" href="true">
-          <img
-            className="project-screenshot"
-            src={studyBuddySc}
-            alt="study-buddy-screenshot"
-            width="100%"
-            border="20px"
-       
-          />
-        </Link>
-      ),
-      
-    },
 
   ];
   return (
-    <div className="container">
+    <> 
+    <swiper-container>
       {projects.map((project,index) => (
-        <ProjectCard key={index} props={project} />
+        <>         
+          <swiper-slide>
+            <ProjectCard key={index} props={project} />
+          </swiper-slide>      
+        </>
       ))}
-    </div>
+    </swiper-container>
+    </>
   );
 }
 
